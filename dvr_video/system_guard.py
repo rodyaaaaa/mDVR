c83d2e27d0e515833677d0921c45d62498ee8e78
rodyaaaaa <rodion6568@gmail.com>
2025-02-14 14:17:17 +0200

    engine: The system_guard module has been added to provide system monitoring. Fix some bugs

--- dvr_video/system_guard.py
+++ dvr_video/system_guard.py
@@ -1,7 +1,11 @@
 import os
 import pathlib
+import time
+
 import psutil
 import asyncio
+import speedtest
+import math
 
 from data.logger import Logger
 
@@ -9,16 +13,34 @@ pathlib.Path("logs/mdvr_system_guard").mkdir(parents=True, exist_ok=True)
 logger = Logger('mdvr_system_guard', "logs/mdvr_system_guard/system_guard.log", 10, "H", 2)
 
 
+async def check_cpu_temp():
+    thermal_zone = 'thermal_zone0'  # Replace with the appropriate thermal zone
+    file_path = f'/sys/class/thermal/{thermal_zone}/temp'
+
+    with open(file_path, 'r') as file:
+        temperature = int(file.read()) / 1000
+
+    return temperature
+
+
+async def network_check():
+    s = speedtest.Speedtest()
+
+    download_speed = s.download()
+    upload_speed = s.upload()
+
+    download_speed = math.floor(download_speed / 1000000)
+    upload_speed = math.floor(upload_speed / 1000000)
+
+    return download_speed, upload_speed
+
+
 async def main():
-        load1, load5, load15 = psutil.getloadavg()
+    temp_cpu = await check_cpu_temp()
+    cpu_persent_usage = psutil.cpu_percent()
+    download_speed, upload_speed = await network_check()
 
-        cpu_usage = (load15 / os.cpu_count()) * 100
-
-        check_temp_cpu = os.popen("cat /sys/class/thermal/thermal_zone0/temp").read()
-
-        check_temp_cpu = int(check_temp_cpu) / 1000
-
-        logger.info(f"System: CPU usage: {cpu_usage} | CPU temp: {check_temp_cpu}")
+    logger.info(f"CPU usage: {cpu_persent_usage}% | CPU temp: {temp_cpu} | Download speed: {download_speed}Mbit/s | Upload speed: {upload_speed}Mbit/s")
 
 
 if __name__ == "__main__":
