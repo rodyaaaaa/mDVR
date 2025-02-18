5f05c349904570a8b1d3b92e83069e9b60f22e3f
rodyaaaaa <rodion6568@gmail.com>
2025-02-18 17:38:24 +0200

    update: deb

--- dvr_video/main.py
+++ dvr_video/main.py
@@ -17,6 +17,7 @@ notifier.notify('READY=1')
 async def async_write_video(current_link, file_name):
     stream = ffmpeg.input(config['camera_list'][current_link], t=str(config['video_options']['video_duration']), rtsp_transport='tcp')
     stream = ffmpeg.filter(stream, 'scale', width=config['rtsp_options']['rtsp_resolution_x'], height=config['rtsp_options']['rtsp_resolution_y'])
+    stream = ffmpeg.filter(stream, 'fps', fps=config['video_options']['fps'])
     stream = ffmpeg.output(stream, f"temp/{current_link+1}24{file_name}.mp4", vcodec="libx264")
     process = ffmpeg.run_async(stream, quiet=True)
 
